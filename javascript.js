1d0ca0eb277738a8c8a274ff415a19f210b1d30e
Phu Koi Ka <devkoika@gmail.com>
2024-05-08 11:33:37 +0700

    test

--- javascript.js
+++ javascript.js
@@ -1,56 +1,36 @@
-// PRODUCT CARD 
-$('.owl-carousel').owlCarousel({
-    autoplay:true,
-    autoplayTimeout:3000,
-    autoplayHoverPause:true,
-    loop:true,
-    margin:10,
-    nav:true,
-    responsive:{
-        0:{
-            items:2
-        },
-        601:{
-            items:3
-        },
-        1000:{
-            items:4
-        }
-    }
-})
-$('.play').on('click',function(){
-    owl.trigger('play.owl.autoplay',[3000])
-})
-$('.stop').on('click',function(){
-    owl.trigger('stop.owl.autoplay')
-})
-
+// PRODUCT CARD
+$(".owl-carousel").owlCarousel({
+  autoplay: true,
+  autoplayTimeout: 3000,
+  autoplayHoverPause: true,
+  loop: true,
+  margin: 10,
+  nav: true,
+  responsive: {
+    0: {
+      items: 2,
+    },
+    601: {
+      items: 3,
+    },
+    1000: {
+      items: 4,
+    },
+  },
+});
+$(".play").on("click", function () {
+  owl.trigger("play.owl.autoplay", [3000]);
+});
+$(".stop").on("click", function () {
+  owl.trigger("stop.owl.autoplay");
+});
 
 // DROP MENU SIDEBAR
-const dropMenu = document.querySelectorAll('.dropdown__menu');
-const dropBtn = document.querySelectorAll('.dropdown__btn');
+const dropMenu = document.querySelectorAll(".dropdown__menu");
+const dropBtn = document.querySelectorAll(".dropdown__btn");
 
-dropBtn.forEach(function(btn, i) {
-    dropBtn[i].addEventListener('click', () => {
-        dropMenu[i].classList.toggle('active')
-    })
-})
-
-// MESSENGER MENU
-const mesBtn = document.querySelector('.mes__btn');
-const menuMes = document.querySelector('.mes__menu');
-const exitIcon = document.querySelector('.fa-minus');
-const menuIcon = document.querySelector('.fa-ellipsis');
-const menuNav = document.querySelector('.mes__menu2');
-
-mesBtn.addEventListener('click', () => {
-    menuMes.classList.toggle('active');
-})
-
-exitIcon.addEventListener('click', () => {
-    menuMes.classList.remove('active');
-})
-
-menuIcon.addEventListener('click', () => {
-    menuNav.classList.toggle('active');
-})
+dropBtn.forEach(function (btn, i) {
+  dropBtn[i].addEventListener("click", () => {
+    dropMenu[i].classList.toggle("active");
+  });
+});
